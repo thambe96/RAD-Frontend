20fc9f03ec6254810098e1a417f117e3b1543a88
Oshadha Thambavita <osthambe20@gmail.com>
2025-12-10 00:46:51 +0530

    Intial commit for the frontend

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='bg-linear-to-r from-pink-500 to-violet-500 
+                      bg-clip-text 
+                      text-5xl font-extrabold 
+                      text-transparent'>
+                        Start the react frontend
+                      </h1>
+
+    </>
+  )
+}
+
+export default App
