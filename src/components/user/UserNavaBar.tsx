5c9afa23851f00151f3f472ab373d6ea1118ecdc
Oshadha Thambavita <osthambe20@gmail.com>
2026-01-05 23:23:55 +0530

    Add conditional rendering for NavLinks based on user status

--- src/components/user/UserNavaBar.tsx
+++ src/components/user/UserNavaBar.tsx
@@ -2,19 +2,23 @@ import { NavLink } from "react-router-dom"
 import UserDetails from "../UserDetails"
 import RequestBtn from "./RequestBtn"
 import LogoutBtn from "../LogoutBtn"
+import { useAuth } from "../../context/authContext"
 
 export default function UserNavaBar() {
 
     const navLinkStyels: string = "hover:bg-purple-300 hover:text-white cursor-pointer"
+    const {user} = useAuth()
+
+
 
   return (
     <div className="w-full flex justify-evenly items-center bg-purple-200">
         <div className="flex justify-evenly items-center gap-5">
             <NavLink to={'/userLayout/userHome'}  className={navLinkStyels}>HOME</NavLink>
             <NavLink to={'/userLayout/contributors'} className={navLinkStyels}>CONTRIBUTORS</NavLink>
-            <NavLink to={'/userLayout/wishlist'} className={navLinkStyels}>WISHLIST</NavLink>
-            <NavLink to={'/userLayout/myreviews'} className={navLinkStyels}>MYREVIEWS</NavLink>
-            <NavLink to={'/userLayout/addreview'}className={navLinkStyels}>ADDREVIES</NavLink>
+            {user?.status === "DEFAULT" && <NavLink to={'/userLayout/wishlist'} className={navLinkStyels}>WISHLIST</NavLink>}
+            {user?.status === "APPROVED" && <NavLink to={'/userLayout/myreviews'} className={navLinkStyels}>MYREVIEWS</NavLink>}
+            {user?.status === "APPROVED" &&  <NavLink to={'/userLayout/addreview'}className={navLinkStyels}>ADDREVIES</NavLink>}
         </div>
         <div>
             <RequestBtn />
